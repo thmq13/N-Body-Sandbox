6a28a44b61f771a6226757dac22c15223a27aa8d
thmq13 <ddajt12@gmail.com>
2026-07-06 15:45:58 +0700

    Minor tweaks to types.

--- N-Body Sandbox/include/Config.hpp
+++ N-Body Sandbox/include/Config.hpp
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <variant>
+#include <cstddef>
+#include <cstdint>
 
 struct ComputeConfig {
     enum class Algorithm { DirectN2, BarnesHut };
@@ -31,26 +33,23 @@ struct SpawnerConfig {
     struct UniformSphereParams {
         double radius{ 10.0 };
         double totalMass{ 1000.0 };
-        size_t bodyCount{ 100 };
+        std::size_t bodyCount{ 100 };
     };
-
     struct PlummerParams {
         double scaleRadius{ 5.0 };
         double totalMass{ 1000.0 };
-        size_t bodyCount{ 200 };
+        std::size_t bodyCount{ 200 };
     };
-
     struct DiskGalaxyParams {
         double radius{ 15.0 };
         double thickness{ 0.5 };
         double totalMass{ 2000.0 };
-        size_t bodyCount{ 300 };
+        std::size_t bodyCount{ 300 };
     };
-
     using ShapeParams = std::variant<UniformSphereParams, PlummerParams, DiskGalaxyParams>;
 
     struct ClusterConfig {
-        std::size_t id;
+        std::uint32_t id;
         ShapeParams params;
     };
 

679c52e43e7711c09d45c1575059b92f8bb6cb89
thmq13 <ddajt12@gmail.com>
2026-07-16 16:55:18 +0700

    small tweaks

--- N-Body Sandbox/include/NBodySandboxApplication.hpp
+++ N-Body Sandbox/include/NBodySandboxApplication.hpp
@@ -1,43 +1,41 @@
-    #pragma once
+#pragma once
 
-    #include <atomic>
-    #include <memory>
-    #include <thread>
+#include <atomic>
+#include <memory>
 
-    #include <Core/Events.hpp>
-    #include <AppState.hpp>
+#include <Core/Message.hpp>
+#include <Core/AppState.hpp>
 
-    class MessageBus;
-    class PhysicsEngine;
-    class RenderingEngine;
-    class UIManager;
-    class SimulationStateBuffer;
+class MessageBus;
+class GeneratorEngine;
+class PhysicsEngine;
+class RenderingEngine;
+class UIManager;
+class ParticleBuffer;
 
-    class NBodySandboxApplication {
-    public:
-        NBodySandboxApplication();
+class NBodySandboxApplication {
+public:
+    NBodySandboxApplication();
 
-        ~NBodySandboxApplication();
+    ~NBodySandboxApplication();
 
-        NBodySandboxApplication(const NBodySandboxApplication&) = delete;
-        NBodySandboxApplication& operator=(const NBodySandboxApplication&) = delete;
-        NBodySandboxApplication(NBodySandboxApplication&&) = delete;
-        NBodySandboxApplication& operator=(NBodySandboxApplication&&) = delete;
+    NBodySandboxApplication(const NBodySandboxApplication&) = delete;
+    NBodySandboxApplication& operator=(const NBodySandboxApplication&) = delete;
+    NBodySandboxApplication(NBodySandboxApplication&&) = delete;
+    NBodySandboxApplication& operator=(NBodySandboxApplication&&) = delete;
 
-        void setUpAndGenerateScene();
-        void executeMasterLoop();
-    private:
-        std::atomic<bool> m_isRunning{ true };
-        std::atomic<AppState> m_appState{ AppState::TitleScreen };
+    void setUpAndGenerateScene();
+    void executeMasterLoop();
+private:
+    std::atomic<bool> m_isRunning{ true };
+    std::atomic<AppState> m_appState{ AppState::TitleScreen };
 
-        std::unique_ptr<MessageBus> m_messageBus{};
+    std::unique_ptr<MessageBus> m_messageBus{};
+    std::shared_ptr<ParticleBuffer> m_particleBuffer{};
+    std::unique_ptr<GeneratorEngine> m_generatorEngine{};
+    std::unique_ptr<PhysicsEngine> m_physicsEngine{};
+    std::unique_ptr<RenderingEngine> m_renderingEngine{};
+    std::unique_ptr<UIManager> m_uiManager{};
 
-        std::shared_ptr<SimulationStateBuffer> m_simulationStateBuffer{};
-
-        std::unique_ptr<PhysicsEngine> m_physicsEngine{};
-        std::unique_ptr<RenderingEngine> m_renderingEngine{};
-        std::unique_ptr<UIManager> m_uiManager{};
-
-
-        void handleMessage(const SystemMessage& message);
-    };
+    void handleMessage(const SystemMessage& message);
+};

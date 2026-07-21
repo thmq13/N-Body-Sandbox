c23aa94b79564ce9d20a5948d1efb32976e22bb1
thmq13 <ddajt12@gmail.com>
2026-07-21 17:16:28 +0700

    Clean up the code

--- N-Body Sandbox/include/UI/SchemaRenderer.hpp
+++ N-Body Sandbox/include/UI/SchemaRenderer.hpp
@@ -2,9 +2,7 @@
 
 namespace NBody::Core { struct ParameterSchema; }
 
-namespace NBody::UI {
-    namespace SchemaRenderer {
-        void drawInput(Core::ParameterSchema& schema, float fieldWidth = 100.0f);
-        void drawDisplay(const Core::ParameterSchema& schema);
-    }
+namespace NBody::UI::SchemaRenderer {
+    void drawInput(Core::ParameterSchema& schema, float fieldWidth = 100.0f);
+    void drawDisplay(const Core::ParameterSchema& schema);
 }
